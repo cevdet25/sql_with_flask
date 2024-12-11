077f9b4e42dc7e322c02b71e64ddfef2a9128fb2
Cevdet <cevdetuslu25@gmail.com>
2024-12-11 11:33:40 +0300

    rds endpoint changed

--- flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
+++ flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
@@ -7,7 +7,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # Configure mysql database
-app.config['MYSQL_DATABASE_HOST'] = 'cevdet-aws-19.czkcy8iuycbj.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_HOST'] = 'cevdet-mysql-db.czkcy8iuycbj.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
 app.config['MYSQL_DATABASE_DB'] = 'clarusway'
